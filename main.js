a8680971934425a3fba3aa62e3f1fe5d2ba96d34
ElshanNadiev13 <platin618@gmail.com>
2023-05-01 18:12:23 +0400

    init

---
+++ main.js
@@ -0,0 +1,10 @@
+const a = document.querySelector(".a1");
+
+a.addEventListener('submit', (e) => {
+    const b = document.querySelector("ul");
+    const d = new FormData(e.target);
+    const todo = d.get('a3');
+    const y = document.createElement('li');
+    y.innerText=
+});
+
